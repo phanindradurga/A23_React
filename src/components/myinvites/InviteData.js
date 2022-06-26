2e5fb6ac5673adecabba5b849cc55195748a31be
Phanindra <phanidevlancer@gmail.com>
2022-06-26 10:03:21 +0530

    UI changes

---
+++ src/components/myinvites/InviteData.js
@@ -0,0 +1,9 @@
+export class InviteData {
+    constructor(isSelected, emailMob, lastInvite, acceptDate, status) {
+        this.isSelected = isSelected
+        this.emailMob = emailMob
+        this.lastInvite = lastInvite
+        this.acceptDate = acceptDate
+        this.status = status
+    }
+}
--- src/components/myinvites/MyInvitesList.js
+++ src/components/myinvites/MyInvitesList.js
@@ -1,8 +1,20 @@
+import Checkbox from '@material-ui/core/Checkbox';
+import CircleChecked from '@material-ui/icons/CheckCircleOutline';
+import CircleCheckedFilled from '@material-ui/icons/CheckCircle';
+import CircleUnchecked from '@material-ui/icons/RadioButtonUnchecked';
+import { InviteData } from './InviteData'
 const MyInvitesList = () => {
 
     const headerTitles = ["Select", "Email/Mobile", "Last Invite", "Accept Date", "Status"]
-    const widthPercentage = [15, 25, 20, 25, 15]
-    const records = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    const widthPercentage = [15, 35, 15, 20, 15]
+
+    const data1 = new InviteData(false, "Phani2205@gmail.com", "22nd jun", "NA", "P")
+    const data2 = new InviteData(false, "+919985788376", "22nd jun", "NA", "P")
+    const data3 = new InviteData(false, "+919848109877", "22nd Aug", "NA", "P")
+
+    const records = [data1, data2, data3]
+
+
 
     const styles = {
         mainContainer: {
@@ -25,7 +37,7 @@ const MyInvitesList = () => {
         return (
             <div style={{ ...styles.listheader, marginTop: '1rem', backgroundColor: '#ebbe69', boxSizing: 'border-box', borderColor: '#C9C9C9', borderRadius: '0.2rem', borderStyle: 'solid' }}>
                 {headerTitles.map((title, index) => {
-                    return <span style={{ width: `${widthPercentage[index]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.8rem' }}>{title}</span>
+                    return <span style={{ width: `${widthPercentage[index]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.6rem' }}>{title}</span>
                 })}
             </div>
         )
@@ -36,11 +48,28 @@ const MyInvitesList = () => {
             <div>
                 {records.map((records, index) => {
                     return <div style={{ display: 'flex', width: '100%', boxSizing: 'border-box', backgroundColor: (index % 2 === 0) ? '#FFFFFF' : '#F7F7F7' }}>
-                        {headerTitles.map((title, index) => {
-                            return (
-                                <span style={{ width: `${widthPercentage[index]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.8rem' }}>{title}</span>
-                            )
-                        })}
+                        <div style={{ width: `${widthPercentage[0]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', alignSelf: 'center' }}>
+                            <Checkbox
+                                icon={<CircleUnchecked color='primary' />}
+                                checkedIcon={<CircleChecked color='primary' />
+                                } />
+                        </div>
+                        <div style={{ width: `${widthPercentage[1]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.6rem', alignSelf: 'center', textAlign: 'start' }}>
+                            {records.emailMob}
+                        </div>
+
+                        <div style={{ width: `${widthPercentage[2]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.6rem', alignSelf: 'center' }}>
+                            {records.lastInvite}
+                        </div>
+
+                        <div style={{ width: `${widthPercentage[3]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.6rem', alignSelf: 'center' }}>
+                            {records.acceptDate}
+                        </div>
+
+                        <div style={{ width: `${widthPercentage[4]}%`, paddingTop: '0.5rem', paddingBottom: '0.5rem', fontSize: '0.6rem', alignSelf: 'center' }}>
+                            {records.status}
+                        </div>
+
                     </div>
 
                 })}
